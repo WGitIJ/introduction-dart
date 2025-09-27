56bed936a3312fba85b6eec937ad7b39d74d065d
WGitIJ <wisdomimade@paucasesnovescifp.cat>
2025-09-27 23:15:46 +0200

    Not yet finished, but trying to create a password gennerator

---
+++ Syntax Exercises/exercise8.dart
@@ -0,0 +1,15 @@
+// Escriu un programa que donada una longitud (expressada numericament int),
+// generi una contrasenya segura de la longitud esmentada totalment aleatòria
+// amb caràcters alfanumèrics.
+
+import 'dart:math';
+
+void main(){
+
+}
+
+int createPassword(int length){
+  const alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const specialCharacters = '!@#\$%^&*()_+-=[]{}|;:,.<>?';
+
+}
